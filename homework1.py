bae017964762cc2058d4fb1eeddafdcf584daa71
longlee218 <longlehoang2013@gmail.com>
2020-02-18 11:15:20 +0700

    Long

--- homework1.py
+++ homework1.py
@@ -5,10 +5,22 @@ def sum(listed, k):
         for i in listed:
             if k - i in listed:
                 check = True
-                break
     return check
+
+# cach 2
+def two_sum(listed, k):
+    if len(listed) == k:
+        for i in range(len(listed)):
+            current = listed[i]
+            for j in range(i+1, len(listed)):
+                if current + listed[j] == k:
+                    return True
+    else:
+        return False
+
 
 if __name__ == "__main__":
     listed = [4, 7, 1, -3, 2]
     k = 5
     print(sum(listed, k))
+    print(two_sum(listed, k))
