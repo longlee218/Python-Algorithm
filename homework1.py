faf127e40a690b6079fdeb939c6a2cfb88e5e1f8
longlee218 <longlehoang2013@gmail.com>
2020-02-23 09:05:13 +0700

    fix

--- homework1.py
+++ homework1.py
@@ -1,25 +1,24 @@
-# cach 1
-def sum(listed, k):
+
+def two_sum1(listed, k):
     check = False
-    if len(listed) == k:
-        for i in listed:
-            if k - i in listed:
-                check = True
+    for i in listed:
+        if k - i in listed:
+            check = True
     return check
 
-# cach 2
-def two_sum(listed, k):
-    if len(listed) == k:
-        for i in range(len(listed)):
-            current = listed[i]
-            for j in range(i+1, len(listed)):
-                if current + listed[j] == k:
-                    return True
+
+def two_sum2(listed, k):
+    for i in range(len(listed)):
+        current = listed[i]
+        for j in range(i+1, len(listed)):
+            if current + listed[j] == k:
+                return True
     else:
         return False
 
+
 if __name__ == "__main__":
-    listed = [4, 7, 1, -3, 2]
-    k = 5
-    print(sum(listed, k))
-    print(two_sum(listed, k))
+    array = [4, 7, 1, -3, 2]
+    key = 5
+    print(two_sum1(array, key))
+    print(two_sum2(array, key))
