1084a5b35be4ffe42c577c29a0f967171e7eec37
longlee218 <longlehoang2013@gmail.com>
2020-02-24 22:22:27 +0700

    Baitap 24/2/2020

--- homework4.py
+++ homework4.py
@@ -3,6 +3,14 @@ def singleNumber(nums):
     return 2*sum(set(nums)) - sum(nums)
 
 
+# def single_number(nums):
+#     for i in nums:
+#         if nums.count(i) == 1:
+#             return i
+#         else:
+#             continue
+
+
 if __name__ == '__main__':
     array = [4, 3, 2, 4, 1, 3, 2]
-    print(singleNumber([4, 3, 2, 4, 1, 3, 2]))
+    print(singleNumber(array))
