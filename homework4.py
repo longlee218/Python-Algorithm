de7e8f6aab94086a26fad200538e279aa28b4946
longlee218 <longlehoang2013@gmail.com>
2020-02-27 20:37:06 +0700

    Bai tap 27/2/2020

--- homework4.py
+++ homework4.py
@@ -4,17 +4,17 @@ def singleNumber(nums):
 
 # solution 2
 
-# def singleNumber2(nums):
-#     array1 = []
-#     array2 = []
-#     for i in nums:
-#         if i in array1:
-#             array1.append(i)
-#         array2.append(i)
-#     result = set(array2).difference(set(array1))
-#     return result.pop()
+def singleNumber2(nums):
+    array1 = []
+    array2 = []
+    for i in nums:
+        if i not in array1:
+            array1.append(i)
+        else:
+            array2.append(i)
+    return (set(array1).difference(set(array2))).pop()
 
 
 if __name__ == '__main__':
-    print(singleNumber([4, 3, 2, 4, 1, 3, 2]))
-    # print(singleNumber2([4, 3, 2, 4, 1, 3, 2]))
+    # print(singleNumber([4, 3, 2, 4, 1, 3, 2]))
+    print(singleNumber2([4, 3, 2, 4, 1, 3, 2]))
