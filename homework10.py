5b337523e6e5beab6f8fb576ca085265ce286ab4
longlee218 <longlehoang2013@gmail.com>
2020-03-07 23:49:34 +0700

    Bai tap 7-3-2020

--- homework10.py
+++ homework10.py
@@ -11,7 +11,7 @@
 """
 
 
-def stripe_question(nums):
+def stripe_question1(nums):
     max_number = 0
     for i in nums:
         if i > max_number:
@@ -31,17 +31,38 @@ def stripe_question(nums):
         return max_number+1
 
 
+def stripe_question2(nums):
+    for i in range(len(nums)):
+        if nums[i] > len(nums) or nums[i] < 0:
+            continue
+        temp = nums[i]
+        if nums[temp-1] != nums[i]:
+            nums[temp-1], nums[i] = nums[i], nums[temp-1]
+        else:
+            continue
+    for i in range(len(nums)):
+        if i+1 != nums[i]:
+            return i+1
+    return len(nums)+1
+    # return nums
+
+
 if __name__ == '__main__':
     array1 = [3, 2, -1, 1, 2]                      # expect 4
     array2 = [1, 2, 0]                             # expect 3
     array3 = [0, -1, 0]                            # expect 1
     array4 = [1, 4, 2, 7, 3, -1, 1, 4, 2]          # expect 5
-    print(stripe_question(array4))
+    print(stripe_question1(array4))
+    print(stripe_question2(array4))
 
 """
-
+Solution1:
     +) Time complexity O(n+m) <n: len of nums, m: max_number in nums>    
     +) Memory space O(m) <m: max_number in nums>
+    
+Solution2:
+    +) Time complexity O(n) <n: len of nums>
+    +) Memory space O(1)    
 --------------------------------------------------------------------------
 làm ơn good đi mà, làm ơn pls :((
 
